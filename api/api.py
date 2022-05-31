9961bfbf2e6136e2b25de043ed91dcd7638fc4b3
Stephan <zz.zx100@mail.ru>
2022-05-31 20:35:27 +0300

    delete death code

--- api/api.py
+++ api/api.py
@@ -12,8 +12,6 @@ WEBHOOK = os.getenv("WEBHOOK")
 
 @app.post("/api/deal/")
 async def deals(deal: BitrixDeal):
-    # bitrix_contact = BitrixContact(bitrix_contact, deal.client)
-    # client = BitrixContact(deal.client)
     deal.client.id = await deal.client.find_contact(deal.client)
     if deal.client.id:
         message = await deal.add_deal(deal)
@@ -27,4 +25,3 @@ async def deals(deal: BitrixDeal):
 async def tasks(task: BitrixTask):
     response = await task.add_task(task=task)
     return response
-    # return {"status": "501", "message": "Server error"}
